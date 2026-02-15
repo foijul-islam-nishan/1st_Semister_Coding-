a129d8c94fc22af57d56dc37b154c724009f8fcc
foijul-islam-nishan <c261125@ugrad.iiuc.ac.bd>
2026-02-15 13:04:32 +0600

    Add files via upload

---
+++ EscapeSequence.c
@@ -0,0 +1,10 @@
+/*
+Write a Program using some escape sequence.
+*/
+#include<stdio.h>
+int main(){
+printf("\a  \n Foijul Islam Nishan");
+printf("\n International Islamic University");
+printf("\n 01613114063");
+getchar();
+}
