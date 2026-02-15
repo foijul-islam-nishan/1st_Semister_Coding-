d018132d5c81e6a7d0bd2dc0fe5529efab492712
foijul-islam-nishan <c261125@ugrad.iiuc.ac.bd>
2026-02-15 22:07:45 +0600

    Add files via upload

---
+++ Veriable-declaration-initialization.c
@@ -0,0 +1,19 @@
+/*
+declare some variable and initialize
+*/
+#include<stdio.h>
+int main(){
+//variable declaration
+int num1;
+int num2;
+float num3;
+double num4;
+char ch;
+//variable initialization
+num1=10;
+num2=11;
+num3=111.11;
+num4=20.2020202020;
+ch='a';
+getchar();
+}
