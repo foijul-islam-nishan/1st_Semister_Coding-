807c2413a4f3e50480e4cef5b0604727736e7b4d
foijul-islam-nishan <c261125@ugrad.iiuc.ac.bd>
2026-02-15 12:28:44 +0600

    Add files via upload

---
+++ Multiple_Line_comment.c
@@ -0,0 +1,11 @@
+/*
+Write a Program that shows your Name and you age in a new line
+*/
+#include<stdio.h>
+int main() {
+           printf("My name is Foijul Islam Nishan \n and my age is 21");
+
+           getchar();
+
+
+}
