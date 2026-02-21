a9c61efc8b4db3ecd3943136c86575cb94213fdb
foijul-islam-nishan <c261125@ugrad.iiuc.ac.bd>
2026-02-22 00:51:32 +0600

    Add files via upload

---
+++ Print-float-double-and-character.c
@@ -0,0 +1,14 @@
+/*Write a program that prints float
+                                                double and
+                                                charcter*/
+#include<stdio.h>
+int main(){
+                 float a = 11.11;
+                 double b= 34.4433;
+                 char ch ='v';
+                printf("Float number is %.2f\n",a);
+                printf("Double number is %lf\n",b);
+                printf("Character is %c\n",ch);
+                getchar();
+
+}
